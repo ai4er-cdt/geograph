935141f5c5e7bd0740a9c18fc832e1f6ee74193a
Simon Mathis <svm34@cam.ac.uk>
2021-01-22 11:07:44 +0000

    feature: add several useful paths to constants

--- src/constants.py
+++ src/constants.py
@@ -5,8 +5,18 @@ source folder, the project path, etc.
 
 # Place all your constants here
 import os
+import pathlib
 
 # Note: constants should be UPPER_CASE
 constants_path = os.path.realpath(__file__)
 SRC_PATH = os.path.dirname(constants_path)
 PROJECT_PATH = os.path.dirname(SRC_PATH)
+
+# Data directory on GWS
+GWS_DATA_DIR = pathlib.Path(
+    "/gws/nopw/j04/ai4er/guided-team-challenge/2021/biodiversity"
+)
+# CEDA directory
+CEDA_DIR = pathlib.Path("/neodc")
+# ESA Landcover directory in CEDA
+ESA_LANDCOVER_DIR = CEDA_DIR / "esacci/land_cover/data/land_cover_maps/v2.0.7"
