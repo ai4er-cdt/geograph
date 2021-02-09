c721e97f2d5f8843e4b19e7e822e7d32044b0249
rdnfn <75615911+rdnfn@users.noreply.github.com>
2021-02-09 17:08:24 +0000

    aux: add GWS data dir to constants

--- src/constants.py
+++ src/constants.py
@@ -5,6 +5,7 @@ source folder, the project path, etc.
 
 # Place all your constants here
 import os
+import pathlib
 from pyproj import Transformer
 
 # Note: constants should be UPPER_CASE
@@ -12,8 +13,10 @@ constants_path = os.path.realpath(__file__)
 SRC_PATH = os.path.dirname(constants_path)
 PROJECT_PATH = os.path.dirname(SRC_PATH)
 
+# Jasmine data dir
+GWS_DATA_DIR = pathlib.Path("/gws/nopw/j04/ai4er/guided-team-challenge/2021/biodiversity")
 
-
+# Coordinate reference systems and locations
 WGS84_CODE = 'EPSG:4326'
 UTM35N_CODE = 'EPSG:32635' # coordinate system of chernobyl data
 transformer = Transformer.from_crs(WGS84_CODE, UTM35N_CODE)
