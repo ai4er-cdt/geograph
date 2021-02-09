842d91448cb5ae196dd3739cb670fc51a420e965
rdnfn <75615911+rdnfn@users.noreply.github.com>
2021-02-09 12:18:12 +0000

    aux: add chernobyl coords to constants

--- src/constants.py
+++ src/constants.py
@@ -5,8 +5,17 @@ source folder, the project path, etc.
 
 # Place all your constants here
 import os
+from pyproj import Transformer
 
 # Note: constants should be UPPER_CASE
 constants_path = os.path.realpath(__file__)
 SRC_PATH = os.path.dirname(constants_path)
 PROJECT_PATH = os.path.dirname(SRC_PATH)
+
+
+
+WGS84_CODE = 'EPSG:4326'
+UTM35N_CODE = 'EPSG:32635' # coordinate system of chernobyl data
+transformer = Transformer.from_crs(WGS84_CODE, UTM35N_CODE)
+CHERNOBYL_COORDS_WGS84 = (51.389167, 30.099444) # coordinates of chernobyl power reactor
+CHERNOBYL_COORDS_UTM35N = transformer.transform(*CHERNOBYL_COORDS_WGS84)
