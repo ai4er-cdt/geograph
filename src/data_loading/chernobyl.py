7e9e8a1e8732b51f365fb72efef061068df0a95b
rdnfn <75615911+rdnfn@users.noreply.github.com>
2021-02-12 11:40:27 +0000

    tidy: rename Polesia data loading functions

--- src/data_loading/chernobyl.py
+++ src/data_loading/chernobyl.py
@@ -5,12 +5,27 @@ This module provides helper functions to load land cover data close to chernobyl
 import geopandas as gpd
 from src.constants import GWS_DATA_DIR
 
-def get_bio_data():
-    # Getting biotope data
-    bio_path = GWS_DATA_DIR / "chernobyl_habitat_data" / "Biotope_EUNIS_ver1.shp"
-    return gpd.read_file(bio_path)
 
-def get_veg_data():
-    # Getting vegetation data
-    veg_path = GWS_DATA_DIR / "chernobyl_habitat_data" / "Vegetation_mape.shp"
-    return gpd.read_file(veg_path)
+def get_polesia_data(variant: str = "biotope") -> gpd.GeoDataFrame:
+    """
+    This function loads the Polesia landcover data from shared workspace on Jasmin.
+    Note that this requires access to Jasmin and the relevant shared workspaces.
+
+    Args:
+        variant (str, optional): either "biotope" or "vegetation".
+            Defaults to "biotope".
+
+    Returns:
+        gpd.GeoDataFrame: shape file data loaded as polygons in data frame
+    """
+
+    if variant == "biotope":
+        data_path = GWS_DATA_DIR / "chernobyl_habitat_data" / "Biotope_EUNIS_ver1.shp"
+    elif variant == "vegetation":
+        data_path = GWS_DATA_DIR / "chernobyl_habitat_data" / "Vegetation_mape.shp"
+    else:
+        raise ValueError(
+            "The variant does not match any existing Polesia data variant."
+        )
+
+    return gpd.read_file(data_path)
