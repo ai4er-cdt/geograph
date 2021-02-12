b1f07be6b8d4d757b3d034743a32889cc016a9a9
rdnfn <75615911+rdnfn@users.noreply.github.com>
2021-02-12 12:59:40 +0000

    doc: add missing standard docstrings and type annotations

--- src/data_loading/test_data.py
+++ src/data_loading/test_data.py
@@ -1,7 +1,8 @@
 """
-This file contains functions that supply simple test data.
+This module contains functions that supply simple test data.
 """
 
+from typing import Tuple
 
 import geopandas as gpd
 import shapely
@@ -10,13 +11,30 @@ from src.constants import CHERNOBYL_COORDS_UTM35N, UTM35N
 
 
 def get_square_row(
-    num_squares=2, translation=(2, 0), square_side_len=1, origin=(0, 0), crs=UTM35N
-):
+    num_squares: int = 2,
+    translation: Tuple[float, float] = (2, 0),
+    square_side_len: float = 1,
+    origin: Tuple[float, float] = (0, 0),
+    crs: str = UTM35N,
+) -> gpd.GeoDataFrame:
     """
     Creates a row of squares with side length `square_side_len`,
     each square is translated to the previous by `translation`.
-    Starts with the bottom left corner of first square at
-    `origin`. Returns gpd GeoDataFrame with square polygons.
+    Starts with the bottom left corner of first square at `origin`.
+
+    Args:
+        num_squares (int, optional): number of squares. Defaults to 2.
+        translation (Tuple[float, float], optional): vector that translates one square
+            to the previous. Defaults to (2, 0).
+        square_side_len (float, optional): length of the side of a square.
+            Defaults to 1.
+        origin (Tuple[float, float], optional): coordinates of bottom left corner of
+            first square in row. Defaults to (0, 0).
+        crs (str, optional): coordinate reference system. Should match the one used to
+            to set origin. Defaults to UTM35N.
+
+    Returns:
+        gpd.GeoDataFrame: dataframe that contains the row of squares as polygons
     """
 
     out_gdf = gpd.GeoDataFrame(columns=["id", "geometry", "area"])
@@ -44,9 +62,23 @@ def get_square_row(
     return out_gdf
 
 
-def get_polygon_gdf(name="default"):
-    """
-    Simple function to load polygon test data.
+def get_polygon_gdf(name: str = "squares_apart") -> gpd.GeoDataFrame:
+    """Loads different test cases consisting of rows of squares.
+
+    Args:
+        name (str, optional): test data name. Defaults to "squares_apart".
+            The options are:
+            - "squares_overlapping": overlapping square pair at origin (0,0)
+            - "squares_touching": touching square pair at origin (0,0)
+            - "squares_apart": separated square pair at origin (0,0)
+            - "chernobyl_squares_apart" and "chernobyl_squares_touching": similar to
+                above but with origin at Chernobyl power reactor.
+
+    Raises:
+        ValueError: test data name is not in options above
+
+    Returns:
+        gpd.GeoDataFrame: dataframe with polygon test data
     """
 
     if name == "squares_overlapping":
@@ -73,7 +105,7 @@ def get_polygon_gdf(name="default"):
             origin=CHERNOBYL_COORDS_UTM35N,
             crs=UTM35N,
         )
-    elif name in ["default", "squares_apart"]:
+    elif name == "squares_apart":
         data = get_square_row(
             num_squares=2, translation=(2, 0), square_side_len=1, origin=(0, 0)
         )
