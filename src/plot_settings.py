0740eb60c7595dfd9dfedb495aeea1c9228a394f
Simon Thomas <simon@user-32-199.vpn.net.private.cam.ac.uk>
2021-03-12 17:09:50 +0000

    fix: linted plot_settings.py

--- src/plot_settings.py
+++ src/plot_settings.py
@@ -18,18 +18,20 @@ x=np.linspace(0, np.pi, num=100)
 axs[0, 0].plot(x, np.sin(x)); axs[0, 1].plot(x, np.cos(x))
 axs[1, 0].plot(x, np.sinc(x)); axs[1, 1].plot(x, np.abs(x))
 
-set_dim(fraction_of_line_width=1, subplot=[1, 1], ratio=(5**.5 - 1) / 2)  # set size
-label_subplots(axs, start_from=0, fontsize=10)                            # label subplots
+# set size
+set_dim(fraction_of_line_width=1, subplot=[1, 1], ratio=(5**.5 - 1) / 2)
+
+# label subplots
+label_subplots(axs, start_from=0, fontsize=10)
 
 """
+import numpy as np
 import itertools
-import matplotlib
 from distutils.spawn import find_executable
+import matplotlib
 import matplotlib.style
 import matplotlib.pyplot as plt
-import matplotlib as mpl
 import seaborn as sns
-
 
 
 def ps_defaults(use_tex=True):
@@ -40,33 +42,39 @@ def ps_defaults(use_tex=True):
     """
     #'#CB4154' ,
     # matplotlib.use('agg') this used to be required for jasmin
-    p_general = {"font.family": "serif",
-                 "font.serif": [],
-                # Use 10pt font in plots, to match 10pt font in document
-                "axes.labelsize": 10,
-                "font.size": 10,
-                # Make the legend/label fonts a little smaller
-                "legend.fontsize": 8,
-                "xtick.labelsize": 8,
-                "ytick.labelsize": 8,
-                # Set the font
-                "mathtext.fontset": "cm",
-                "font.family": "STIXGeneral",
-                "font.sans-serif": ["DejaVu Sans"],    # gets rid of error messages
-                "font.monospace": [],
-                "lines.linewidth": 0.75,
-                "scatter.marker": '+',
-                "image.cmap": 'RdYlBu_r',}
+    p_general = {
+        "font.family": "serif",
+        "font.serif": [],
+        # Use 10pt font in plots, to match 10pt font in document
+        "axes.labelsize": 10,
+        "font.size": 10,
+        # Make the legend/label fonts a little smaller
+        "legend.fontsize": 8,
+        "xtick.labelsize": 8,
+        "ytick.labelsize": 8,
+        # Set the font
+        "mathtext.fontset": "cm",
+        # "font.family": "STIXGeneral",
+        "font.sans-serif": ["DejaVu Sans"],  # gets rid of error messages
+        "font.monospace": [],
+        "lines.linewidth": 0.75,
+        "scatter.marker": "+",
+        "image.cmap": "RdYlBu_r",
+    }
     matplotlib.rcParams.update(p_general)
-    matplotlib.style.use('seaborn-colorblind')
+    matplotlib.style.use("seaborn-colorblind")
 
     if use_tex and find_executable("latex"):
-        p_setting = {"pgf.texsystem": "pdflatex",
-                     "text.usetex": True,
-                     "pgf.preamble": r"\usepackage[utf8x]{inputenc} \usepackage[T1]{fontenc} \usepackage[separate -uncertainty=true]{siunitx}"
-                     }
+        p_setting = {
+            "pgf.texsystem": "pdflatex",
+            "text.usetex": True,
+            "pgf.preamble": (r"\usepackage[utf8x]{inputenc} \usepackage[T1]{fontenc}"
+                             +r"\usepackage[separate -uncertainty=true]{siunitx}"),
+        }
     else:
-        p_setting = {"text.usetex": False,}
+        p_setting = {
+            "text.usetex": False,
+        }
     matplotlib.rcParams.update(p_setting)
 
 
@@ -87,7 +95,6 @@ def label_subplots(axs, start_from=0, fontsize=10):
     for i in range(len(axs.ravel())):
         subset_labels.append(orig_label_list[i + start_from])
     for i, label in enumerate(subset_labels):
-        print(i, label)
         axs.ravel()[i].text(
             0.02,
             0.95,
@@ -100,20 +107,20 @@ def label_subplots(axs, start_from=0, fontsize=10):
         )
 
 
-def get_dim(fraction_of_line_width=1, subplot=[1, 1], ratio=(5**.5 - 1) / 2):
-    """ Get aesthetic figure dimensions to avoid scaling in latex.
-​
-    Parameters
-    ----------
-    width: float
-            Width in pts
-    fraction: float
-            Fraction of the width which you wish the figure to occupy
-​
-    Returns
-    -------
-    fig_dim: tuple
-            Dimensions of figure in inches
+def get_dim(fraction_of_line_width=1, subplot=[1, 1], ratio=(5 ** 0.5 - 1) / 2):
+    """Get aesthetic figure dimensions to avoid scaling in latex.
+    ​
+        Parameters
+        ----------
+        width: float
+                Width in pts
+        fraction: float
+                Fraction of the width which you wish the figure to occupy
+    ​
+        Returns
+        -------
+        fig_dim: tuple
+                Dimensions of figure in inches
     """
     thesis_width = 697.3  # roughly 6.5 inches
 
@@ -123,37 +130,48 @@ def get_dim(fraction_of_line_width=1, subplot=[1, 1], ratio=(5**.5 - 1) / 2):
     # Convert from pt to inches
     inches_per_pt = 1 / 72.27
 
-    # Golden ratio to set aesthetic figure height
-    golden_ratio = (5**.5 - 1) / 2
+    # Golden ratio to set aesthetic figure height]
 
     # Figure width in inches
     fig_width_in = fig_width_pt * inches_per_pt
     # Figure height in inches
-    fig_height_in = fig_width_in * golden_ratio * (subplot[0] / subplot[1])
+    fig_height_in = fig_width_in * ratio * (subplot[0] / subplot[1])
 
     fig_dim = (fig_width_in, fig_height_in)
 
     return fig_dim
 
 
-def set_dim(fraction_of_line_width=1, subplot=[1, 1], ratio=(5**.5 - 1) / 2):
-    """ Set aesthetic figure dimensions to avoid scaling in latex.
-​
-    Parameters
-    ----------
-    width: float
-            Width in pts
-    fraction: float
-            Fraction of the width which you wish the figure to occupy
-​
-    Returns: void; alters current figure to have the desired dimensions
-    """    
+def set_dim(fraction_of_line_width=1, subplot=[1, 1], ratio=(5 ** 0.5 - 1) / 2):
+    """Set aesthetic figure dimensions to avoid scaling in latex.
+    ​
+        Parameters
+        ----------
+        width: float
+                Width in pts
+        fraction: float
+                Fraction of the width which you wish the figure to occupy
+    ​
+        Returns: void; alters current figure to have the desired dimensions
+    """
     fig = plt.gcf()
-    fig.set_size_inches(get_dim(fraction_of_line_width=fraction_of_line_width, 
-                                subplot=subplot, ratio=ratio))
+    fig.set_size_inches(
+        get_dim(
+            fraction_of_line_width=fraction_of_line_width,
+            subplot=subplot, ratio=ratio
+        )
+    )
 
 
-_paper_colors_abbv = ["#4d2923ff", "#494f1fff", "#38734bff", "#498489ff", "#8481baff", "#c286b2ff", "#d7a4a3ff"]
+_paper_colors_abbv = [
+    "#4d2923ff",
+    "#494f1fff",
+    "#38734bff",
+    "#498489ff",
+    "#8481baff",
+    "#c286b2ff",
+    "#d7a4a3ff",
+]
 _paper_colors = sns.color_palette(_paper_colors_abbv)
 # sns.palplot(_paper_colors)
 PALETTE = itertools.cycle(_paper_colors)
