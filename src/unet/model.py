fe20a6edb757ee73403c1e60f898b99aaca0b523
Herbie Bradley <mail@herbiebradley.com>
2021-03-12 13:02:54 +0000

    feature: Added dataloader to model.py

--- src/unet/model.py
+++ src/unet/model.py
@@ -3,6 +3,13 @@ import pytorch_lightning as pl
 import segmentation_models_pytorch as smp
 import segmentation_models_pytorch.losses as losses
 import torch
+from src.constants import GWS_DATA_DIR
+from src.unet.dataloader import LabelledSatelliteDataset
+from torch.utils.data import DataLoader
+
+SENTINEL_DIR = GWS_DATA_DIR / "sentinel2_data"
+SENTINEL_POLESIA_DIR = SENTINEL_DIR / "Polesia_10m"
+SENTINEL_CHERNOBYL_DIR = SENTINEL_DIR / "Chernobyl_10m"
 
 
 def get_unet_model(config):
@@ -64,10 +71,40 @@ class UNetModel(pl.LightningModule):
         return self.validation_step(batch, batch_idx)
 
     def train_dataloader(self):
-        raise NotImplementedError
+        """Load train dataset."""
+        images_path = SENTINEL_POLESIA_DIR / "train"
+        labels_path = GWS_DATA_DIR / "polesia_burned_superclasses_all_touched_10m.tif"
+        rgb_bands = [2, 1, 0]
+        train_set = LabelledSatelliteDataset(
+            images_path=images_path, labels_path=labels_path, use_bands=rgb_bands
+        )
+        # Use `pin_memory=True` here for asynchronous data transfer to the GPU,
+        # speeding up data loading.
+        dataloader = DataLoader(
+            dataset=train_set,
+            batch_size=self.config.batch_size,
+            num_workers=self.config.num_workers,
+            pin_memory=True,
+        )
+        return dataloader
 
     def val_dataloader(self):
-        raise NotImplementedError
+        """Load validation dataset."""
+        images_path = SENTINEL_POLESIA_DIR / "train"
+        labels_path = GWS_DATA_DIR / "polesia_burned_superclasses_all_touched_10m.tif"
+        rgb_bands = [2, 1, 0]
+        train_set = LabelledSatelliteDataset(
+            images_path=images_path, labels_path=labels_path, use_bands=rgb_bands
+        )
+        # Best practice to use shuffle=False for validation and testing.
+        dataloader = DataLoader(
+            dataset=train_set,
+            batch_size=8,
+            shuffle=False,
+            num_workers=self.config.num_workers,
+            pin_memory=True,
+        )
+        return dataloader
 
     def test_dataloader(self):
-        raise NotImplementedError
+        return self.val_dataloader()
