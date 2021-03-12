a6a7846e5f9571c7bac2a7c2ec196719a6751122
Simon Mathis <svm34@cam.ac.uk>
2021-03-12 01:23:20 +0000

    feature: add normalizer for imagenet

---
+++ src/unet/normalizers.py
@@ -0,0 +1,41 @@
+"""This module defines normalizers for Satellite image data for UNet training"""
+from abc import ABC, abstractmethod
+import numpy as np
+import torchvision.transforms as transforms
+from torch import Tensor
+
+
+class NormalizerABC(ABC):
+    """Normalizer abstract base class.
+    Serves as skeleton for the other Normalizer classes which are used in the project.
+    Exposes the normalize abstract method which must be implemented by the subclasses.
+    """
+
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def normalize(self, image: np.ndarray, **kwargs) -> Tensor:
+        pass
+
+
+class ImagenetNormalizer:
+    """Imagenet normalizer that standardizes the input image using the ImageNet mean
+    and std values.
+
+    Values are taken from https://pytorch.org/docs/stable/torchvision/models.html
+    """
+
+    def __init__(self):
+        self.dataset_mean = np.array([0.485, 0.456, 0.406])
+        self.dataset_std = np.array([0.229, 0.224, 0.225])
+
+        std_normalize = transforms.Normalize(
+            mean=self.dataset_mean, std=self.dataset_std
+        )
+        self.transform = transforms.Compose([transforms.ToTensor(), std_normalize])
+
+    # pylint: disable=unused-argument
+    def normalize(self, image: np.ndarray, **kwargs) -> Tensor:
+        normalized_image = self.transform(image)
+        return normalized_image
