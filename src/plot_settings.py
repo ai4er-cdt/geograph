16fdbcd8a933542bf5d3c3efb4ecbf6441f3e5de
Simon Thomas <simon@user-32-234.vpn.net.private.cam.ac.uk>
2021-03-14 14:03:19 +0000

    fix: added suggestions, fixed pylint objections

--- src/plot_settings.py
+++ src/plot_settings.py
@@ -4,7 +4,17 @@ plot_settings.py
 
 usage:
 
-from src.plot_settings import ps_defaults, label_subplots, get_dim, set_dim, PALETTE
+from src.plot_settings import (
+    ps_defaults,
+    label_subplots,
+    get_dim,
+    set_dim,
+    PALETTE,
+    STD_CLR_LIST,
+    CAM_BLUE,
+    CAM_RED,
+    OX_BLUE,
+)
 
 ps_defaults(use_tex=True)
 
@@ -13,34 +23,38 @@ ps_defaults(use_tex=True)
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 fig, axs = plt.subplots(2, 2)
-x=np.linspace(0, np.pi, num=100)
-axs[0, 0].plot(x, np.sin(x)); axs[0, 1].plot(x, np.cos(x))
-axs[1, 0].plot(x, np.sinc(x)); axs[1, 1].plot(x, np.abs(x))
+
+x = np.linspace(0, np.pi, num=100)
+axs[0, 0].plot(x, np.sin(x), color=STD_CLR_LIST[0])
+axs[0, 1].plot(x, np.cos(x), color=STD_CLR_LIST[1])
+axs[1, 0].plot(x, np.sinc(x), color=STD_CLR_LIST[2])
+axs[1, 1].plot(x, np.abs(x), color=STD_CLR_LIST[3])
 
 # set size
-set_dim(fraction_of_line_width=1, ratio=(5**.5 - 1) / 2)
+set_dim(fig, fraction_of_line_width=1, ratio=(5 ** 0.5 - 1) / 2)
 
 # label subplots
 label_subplots(axs, start_from=0, fontsize=10)
 
+
 """
+from typing import Sequence, Tuple
 import numpy as np
 import itertools
 from distutils.spawn import find_executable
 import matplotlib
 import matplotlib.style
-import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-def ps_defaults(use_tex: bool=True) -> None:
+def ps_defaults(use_tex: bool = True) -> None:
     """
     Apply plotting style to produce nice looking figures.
     Call this at the start of a script which uses matplotlib,
     and choose the either high (uses latex) or low (without latex)
     """
-    #'#CB4154' ,
     # matplotlib.use('agg') this used to be required for jasmin
     p_general = {
         "font.family": "serif",
@@ -52,9 +66,9 @@ def ps_defaults(use_tex: bool=True) -> None:
         "legend.fontsize": 8,
         "xtick.labelsize": 8,
         "ytick.labelsize": 8,
-        # Set the font
+        # Set the font for maths
         "mathtext.fontset": "cm",
-        # "font.family": "STIXGeneral",
+        # "font.family": "STIXGeneral", # Nice alternative font.
         "font.sans-serif": ["DejaVu Sans"],  # gets rid of error messages
         "font.monospace": [],
         "lines.linewidth": 0.75,
@@ -68,8 +82,10 @@ def ps_defaults(use_tex: bool=True) -> None:
         p_setting = {
             "pgf.texsystem": "pdflatex",
             "text.usetex": True,
-            "pgf.preamble": (r"\usepackage[utf8x]{inputenc} \usepackage[T1]{fontenc}"
-                             +r"\usepackage[separate -uncertainty=true]{siunitx}"),
+            "pgf.preamble": (
+                r"\usepackage[utf8x]{inputenc} \usepackage[T1]{fontenc}"
+                + r"\usepackage[separate -uncertainty=true]{siunitx}"
+            ),
         }
     else:
         p_setting = {
@@ -78,27 +94,37 @@ def ps_defaults(use_tex: bool=True) -> None:
     matplotlib.rcParams.update(p_setting)
 
 
-def label_subplots(axs: Sequence[matplotlib.axes], start_from: int=0, fontsize: int=10) -> None:
-    """
-    Currently adds (a), (b), (c) etc. at the top left of each subplot panel.
-    Labelling order achieved through ravelling the input list/ array
-    :param axs: list or array of subplot axes.
-    :param start_from: lets you start from a different axes.
-    :param fontsize: in points
-    :return: void; alters axes objects
+def label_subplots(
+    axs: Sequence[matplotlib.pyplot.axes],
+    labels: Sequence[str] = [chr(ord("`")+z) for z in range(1, 27)],
+    start_from: int = 0,
+    fontsize: int = 10,
+) -> None:
+    """Adds (a), (b), (c) at the top left of each subplot panel.
+    Labelling order achieved through ravelling the input list / array.
+
+    Args:
+        axs (Sequence[matplotlib.axes]): list or array of subplot axes.
+        labels (Sequence[str]): A sequence of labels for the subplots.
+        start_from (int, optional): skips first ${start_from} labels. Defaults to 0.
+        fontsize (int, optional): Font size for labels. Defaults to 10.
+
+    Returns
+    -------
+        void; alters the `matplotlib.pyplot.axes` objects
+
     """
     if isinstance(axs, list):
         axs = np.asarray(axs)
-    orig_label_list = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"]
-    assert len(axs.ravel()) + start_from <= len(orig_label_list)
+    assert len(axs.ravel()) + start_from <= len(labels)
     subset_labels = []
     for i in range(len(axs.ravel())):
-        subset_labels.append(orig_label_list[i + start_from])
+        subset_labels.append(labels[i + start_from])
     for i, label in enumerate(subset_labels):
         axs.ravel()[i].text(
             0.02,
             0.95,
-            str("(" + label.lower() + ")"),
+            str("(" + label + ")"),
             color="black",
             transform=axs.ravel()[i].transAxes,
             fontsize=fontsize,
@@ -107,8 +133,11 @@ def label_subplots(axs: Sequence[matplotlib.axes], start_from: int=0, fontsize: 
         )
 
 
-def get_dim(fraction_of_line_width: float =1, ratio: float=(5 ** 0.5 - 1) / 2) -> Tuple[float, float]:
-    """Return figure height, width in inches to avoid scaling in latex. Default is golden ratio.
+def get_dim(
+    fraction_of_line_width: float = 1, ratio: float = (5 ** 0.5 - 1) / 2
+) -> Tuple[float, float]:
+    """Return figure height, width in inches to avoid scaling in latex.
+       Default is golden ratio.
     ​
         Parameters
         ----------
@@ -141,29 +170,29 @@ def get_dim(fraction_of_line_width: float =1, ratio: float=(5 ** 0.5 - 1) / 2) -
     return fig_dim
 
 
-def set_dim(fig: matplotlib.pyplot.figure, fraction_of_line_width: float=1, ratio: float=(5 ** 0.5 - 1) / 2) -> None:
+def set_dim(
+    fig: matplotlib.pyplot.figure,
+    fraction_of_line_width: float = 1,
+    ratio: float = (5 ** 0.5 - 1) / 2,
+) -> None:
     """Set aesthetic figure dimensions to avoid scaling in latex.
     ​
         Parameters
         ----------
         fraction_of_line_width: float
-                Fraction of the document width which you wish the figure to occupy
+                Fraction of the document width which you wish the figure to occupy.
 
         ratio: float
-                Fraction of figure width that the figure height should be
+                Fraction of figure width that the figure height should be.
     ​
         Returns: void; alters current figure to have the desired dimensions
     """
-    fig = plt.gcf()
     fig.set_size_inches(
-        get_dim(
-            fraction_of_line_width=fraction_of_line_width,
-            ratio=ratio
-        )
+        get_dim(fraction_of_line_width=fraction_of_line_width, ratio=ratio)
     )
 
 
-_paper_colors_abbv = [
+STD_CLR_LIST = [
     "#4d2923ff",
     "#494f1fff",
     "#38734bff",
@@ -172,6 +201,9 @@ _paper_colors_abbv = [
     "#c286b2ff",
     "#d7a4a3ff",
 ]
-_paper_colors = sns.color_palette(_paper_colors_abbv)
+_paper_colors = sns.color_palette(STD_CLR_LIST)
 # Note: To inspect colors, call `sns.palplot(_paper_colors)`
 PALETTE = itertools.cycle(_paper_colors)
+CAM_BLUE = "#a3c1ad"
+OX_BLUE = "#002147"
+CAM_RED = "#CB4154"
