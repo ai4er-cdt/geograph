5f13f49f9ec8af141f0981b4773422baa2e8a078
Simon Mathis <svm34@cam.ac.uk>
2021-03-24 21:37:16 +0000

    fix: simplify create_node_edge_geometries and update docs

--- src/visualisation/graph_utils.py
+++ src/visualisation/graph_utils.py
@@ -7,43 +7,36 @@ from typing import Tuple
 import geopandas as gpd
 import shapely.geometry
 
-from src.constants import UTM35N
+from src.constants import WGS84
 import src.geograph
 
 
 def create_node_edge_geometries(
     graph: src.geograph.GeoGraph,
-    crs: str = UTM35N,
-    include_edges: bool = True,
-) -> Tuple[gpd.GeoDataFrame, gpd.GeoDataFrame]:
+    crs: str = WGS84,
+) -> Tuple[gpd.GeoSeries, gpd.GeoSeries]:
     """Create node and edge geometries for the networkx graph G.
 
     Returns node and edge geometries in two GeoDataFrames. The output can be used for
     plotting a graph.
 
     Args:
-        graph (nx.Graph): graph with nodes and edges
+        graph (GeoGraph): graph with nodes and edges
         crs (str, optional): coordinate reference system of graph. Defaults to UTM35N.
 
     Returns:
-        Tuple[gpd.GeoDataFrame, gpd.GeoDataFrame]: dataframes of nodes and edges
+        Tuple[gpd.GeoSeries, gpd.GeoSeries]: dataframes of nodes and edges
             respectively.
     """
 
     node_geoms = graph.df.geometry.to_crs(crs).representative_point()
     rep_points = node_geoms.to_dict()
 
-    if include_edges:
-        edge_lines = {}
-        for idx, (node_a, node_b) in enumerate(graph.graph.edges()):
-            point_a = rep_points[node_a]
-            point_b = rep_points[node_b]
-            edge_lines[idx] = shapely.geometry.LineString([point_a, point_b])
-        edge_geoms = gpd.GeoSeries(edge_lines)
-    else:
-        edge_geoms = None
-
-    node_geoms = gpd.GeoDataFrame({"geometry": node_geoms})
-    node_geoms = node_geoms.set_crs(crs)
+    edge_lines = {}
+    for idx, (node_a, node_b) in enumerate(graph.graph.edges()):
+        point_a = rep_points[node_a]
+        point_b = rep_points[node_b]
+        edge_lines[idx] = shapely.geometry.LineString([point_a, point_b])
+    edge_geoms = gpd.GeoSeries(edge_lines)
 
     return node_geoms, edge_geoms
