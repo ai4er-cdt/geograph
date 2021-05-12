fa3c1cf3919af95d869ee1de140bae22d016e504
rdnfn <75615911+rdnfn@users.noreply.github.com>
2021-05-12 10:31:45 +0100

    aux: add nbsphinx link extension to docs config

--- docs/conf.py
+++ docs/conf.py
@@ -38,6 +38,7 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx_autodoc_typehints",
     "nbsphinx",
+    "nbsphinx_link",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
